4d85a8890589a90c3cbc8943017433b37b731a75
punam-kumbhar <punamkumbhar16@gmail.com>
2026-09-11 20:02:35 +0530

    Add Employee Inheritance Mapping Type 1

---
+++ EmployeeInheritanceMappingType1/src/main/java/com/punam/hibernate/EmployeeInheritanceMappingType1/Employee.java
@@ -0,0 +1,59 @@
+package com.punam.hibernate.EmployeeInheritanceMappingType1;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee_inheritance_type1")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id+ ", name=" + name + ", salary=" + salary + "]";
+    }
+}
