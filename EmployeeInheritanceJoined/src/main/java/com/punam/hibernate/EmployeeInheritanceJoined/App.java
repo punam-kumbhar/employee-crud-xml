604564919350752891751472e6aa3b6aa4150900
punam-kumbhar <punamkumbhar16@gmail.com>
2026-09-12 23:55:46 +0530

    Add Hibernate joined inheritance mapping

---
+++ EmployeeInheritanceJoined/src/main/java/com/punam/hibernate/EmployeeInheritanceJoined/App.java
@@ -0,0 +1,31 @@
+package com.punam.hibernate.EmployeeInheritanceJoined;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Configuration cfg = new Configuration().configure();
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Manager manager =new Manager(1, "Punam", 60000, "HR");
+
+        Developer developer =new Developer(2, "Amit", 55000, "Java");
+
+        session.persist(manager);
+        session.persist(developer);
+
+        transaction.commit();
+
+        session.close();
+        factory.close();
+
+        System.out.println("JOINED inheritance data saved successfully!");
+    }
+}
---
+++ EmployeeInheritanceJoined/src/main/java/com/punam/hibernate/EmployeeInheritanceJoined/Developer.java
@@ -0,0 +1,27 @@
+package com.punam.hibernate.EmployeeInheritanceJoined;
+
+public class Developer extends Employee {
+
+    private String programmingLanguage;
+
+    public Developer() {
+    }
+
+    public Developer(int id, String name, double salary, String programmingLanguage) {
+        super(id, name, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public String toString() {
+        return "Developer [programmingLanguage=" + programmingLanguage + "]";
+    }
+}
