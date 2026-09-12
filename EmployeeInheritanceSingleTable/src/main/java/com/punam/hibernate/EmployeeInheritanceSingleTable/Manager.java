57005e2997fa5e3989a80e18d7de1524d0bbf65f
punam-kumbhar <punamkumbhar16@gmail.com>
2026-09-13 00:02:53 +0530

    Add Hibernate single table inheritance mapping

---
+++ EmployeeInheritanceSingleTable/src/main/java/com/punam/hibernate/EmployeeInheritanceSingleTable/Manager.java
@@ -0,0 +1,27 @@
+package com.punam.hibernate.EmployeeInheritanceSingleTable;
+
+public class Manager extends Employee {
+
+    private String department;
+
+    public Manager() {
+    }
+
+    public Manager(int id, String name, double salary, String department) {
+        super(id, name, salary);
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Manager [department=" + department + "]";
+    }
+}
